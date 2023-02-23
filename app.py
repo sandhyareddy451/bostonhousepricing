86ca1d58edc4f395d076f653e76d74c1df2fcd1d
sandhyareddy451 <vemula.sandhya51@gmail.com>
2023-02-23 14:52:57 +0530

    app.py changes

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-from flask import Flask, jsonify, url_for,requests
+import pickle
+from flask import Flask, jsonify, url_for,request, app, render_template
+
+
+
+app=Flask(__name__)
+#load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())[0]).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())[0]).reshape(1,-1))
+if __name__=='__main__':
+    app.run(debug=True)
